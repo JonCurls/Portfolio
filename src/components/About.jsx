2b881533e061f16d77474124ce059d327856051f
MrCurls <JonCurls@gmail.com>
2023-11-26 17:37:40 -0600

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div name="about" className="w-full h-screen bg-bg-[#0a192f]">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>Hi I'm Jonathan, Nice to meet you. Kindly take a look around.</p>
+          </div>
+          <div>I am passionate about bilding excellent software.</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
