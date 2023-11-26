9a299c06125b32deebf2efe3d2e45e2300c16a04
MrCurls <JonCurls@gmail.com>
2023-11-26 12:10:02 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { Navbar } from "./components/Navbar";
+
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Hello World</h1>
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+export const Navbar = () => {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <h1>JF</h1>
+      </div>
+    </div>
+  );
+};
