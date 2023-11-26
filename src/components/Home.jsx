fd7787e8f1f7198f33d6be1596539a3b6d68c350
MrCurls <JonCurls@gmail.com>
2023-11-26 13:12:31 -0600

    intro and work button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,26 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 export const Home = () => {
-  return;
-  <div className="bg-[#0a192f]"></div>;
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hello, My name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Jonathan Franklin
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I am a full-stack developer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]"></p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
